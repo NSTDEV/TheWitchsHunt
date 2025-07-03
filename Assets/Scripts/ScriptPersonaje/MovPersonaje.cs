a7b6fd455977d07b5b336faba649ed8ea48a101b
FerrerasE <elian.ferreras@gmail.com>
2025-07-03 20:10:15 -0300

    collider del enemy y corregir animacion culpa de nahuel

--- Assets/Scripts/ScriptPersonaje/MovPersonaje.cs
+++ Assets/Scripts/ScriptPersonaje/MovPersonaje.cs
@@ -27,6 +27,7 @@ public class MovPersonaje : MonoBehaviour
         // Aplicar movimiento
         transform.position += (Vector3)(inputDir * speed * Time.deltaTime);
 
+        /**
         // Animaciones
         animator.SetBool("MoviendoLado", inputX != 0);
         animator.SetBool("MoviendoArriba", inputY > 0);
@@ -43,11 +44,71 @@ public class MovPersonaje : MonoBehaviour
         {
             Flip();
         }
+        **/
+
+         //animacion de moverse hacia derecha o izquierda
+        if (Input.GetAxis("Horizontal") != 0) // se mueve hacia derecha/
+        {
+            animator.SetBool("MoviendoLado", true); // activar la animacion de caminar
+            animator.SetBool("MoviendoArriba", false);
+            animator.SetBool("QuietoLado", false);
+            animator.SetBool("QuietoArriba", false);
+            animator.SetBool("MoviendoAbajo", false);
+        }
+        else
+        {
+            animator.SetBool("MoviendoLado", false); // desactivar la animacion de caminar
+            animator.SetBool("QuietoLado", true); // el personaje permanece en animacion quieto lado
+            animator.SetBool("QuietoArriba", false);
+        }
+
+
+        //animacion de moverse hacia arriba
+        if (Input.GetAxis("Vertical") > 0) // comparamos el valor del movimiento (-1 / 0 / 1)
+        {
+            animator.SetBool("QuietoArriba", false);
+            animator.SetBool("MoviendoArriba", true); // activar la animacion de caminar
+            animator.SetBool("MoviendoLado", false);
+            animator.SetBool("QuietoLado", false);
+            animator.SetBool("MoviendoAbajo", false);
+        }
+        else
+        {
+            animator.SetBool("QuietoArriba", true); // desactivar la animacion de caminar
+            animator.SetBool("MoviendoArriba", false);
+        }
+
+        //animacion de moverse hacia abajo
+        if (Input.GetAxis("Vertical") < 0)
+        {
+            animator.SetBool("MoviendoAbajo", true); // activar la animacion de caminar
+            animator.SetBool("QuietoArriba", false);
+            animator.SetBool("QuietoLado", false);
+            animator.SetBool("MoviendoLado", false);
+        }
+        else
+        {
+            animator.SetBool("MoviendoAbajo", false); // desactivar la animacion de caminar
+
+        }
+
+        // giro del personaje si se mueve hacia la izquierda
+        if (inputX < 0 && isFacingRight)
+        {
+            Flip();
+        }
+        //giro del personaje si se mueve a la derecha
+        else if (inputX > 0 && !isFacingRight)
+        {
+            Flip();
+        }
     }
 
+    // cambiamos la escala en el eje X para voltear el personaje
     private void Flip()
     {
         transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
         isFacingRight = !isFacingRight;
     }
+
 }
